2e5f29419c6ded10e901ea7451d6068f83fe1b25
Vivek Kumar <viveksingh0143@gmail.com>
2020-02-19 17:25:22 +0530

    Config Token Bug Fixed

--- src/config/comingsoon.php
+++ src/config/comingsoon.php
@@ -2,5 +2,5 @@
 return [
   'start' => env('COMING_ACTIVE', false),
   'subscribe_url' => env('COMING_SUBSCRIBE_URL', null),
-  'tokens' => explode(',', env('COMING_TOKENS', ''))
+  'tokens' => explode(',', env('COMING_TOKENS', null))
 ];
